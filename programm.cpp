7efdd65032a1a13c7251a0a7022c84bc08850c63
AshwiniTiwari996 <ashwinitiwari194@gmail.com>
2026-08-14 09:38:08 +0530

    initial commit

---
+++ programm.cpp
@@ -0,0 +1,39 @@
+// #include<iostream>
+// using namespace std;
+// class point{
+//     private:
+//     int x,y;
+//     public:
+//     point(int x,int y){
+//         this->x=x;
+//         this->y=y;
+//     }
+//    void show(){
+//         cout<<x<<endl;
+//         cout<<y<<endl;
+//     }
+// };
+// int main(){
+//     int a,b;
+//     cin>>a;
+//     cin>>b;
+//     point p(a,b);
+//     p.show();
+// }
+{
+    "C++ Main Template" : {
+        "prefix" : "main",
+        "body" : [
+            "#include<iostream>",
+            "",
+            "using namespace std;"
+            "",
+            "",
+            "int main(){"
+            "",
+            "return 0;"
+            "}",
+        ],
+        "description" : "C++ main template"
+    }
+}
