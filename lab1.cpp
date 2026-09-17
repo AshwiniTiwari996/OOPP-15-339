676bc0b2219faf61165d9d1beb9ba06a7ca39d3c
AshwiniTiwari996 <ashwinitiwari194@gmail.com>
2026-09-17 22:44:39 +0530

    lab1exp

--- lab1.cpp
+++ lab1.cpp
@@ -1,53 +1,96 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// class Student
-// {
-//     string name;
-//     int roll;
-//     string branch;
+class Student
+{
+    string name;
+    int roll;
+    string branch;
 
-// public:
-//     void input()
-//     {
-//         cout << "Enter name: ";
-//         cin >> name;
+public:
+    void input()
+    {
+        cout << "Enter name: ";
+        cin >> name;
 
-//         cout << "Enter roll number: ";
-//         cin >> roll;
+        cout << "Enter roll number: ";
+        cin >> roll;
 
-//         cout << "Enter branch: ";
-//         cin >> branch;
-//     }
+        cout << "Enter branch: ";
+        cin >> branch;
+    }
 
-//     void show()
-//     {
-//         cout << "Name: " << name << endl;
-//         cout << "Roll Number: " << roll << endl;
-//         cout << "Branch: " << branch << endl;
-//     }
-// };
+    void show()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Roll Number: " << roll << endl;
+        cout << "Branch: " << branch << endl;
+    }
+};
 
-// int main()
-// {
-//     Student s1, s2;
+int main()
+{
+    Student s1, s2;
 
-//     cout << "Enter details of Student 1:" << endl;
-//     s1.input();
+    cout << "Enter details of Student 1:" << endl;
+    s1.input();
 
-//     cout << "\nEnter details of Student 2:" << endl;
-//     s2.input();
+    cout << "\nEnter details of Student 2:" << endl;
+    s2.input();
 
-//     cout << " Student 1 Details" << endl;
-//     s1.show();
+    cout << " Student 1 Details" << endl;
+    s1.show();
 
-//     cout << " Student 2 Details" << endl;
-//     s2.show();
+    cout << " Student 2 Details" << endl;
+    s2.show();
 
-//     return 0;
-// }
+    return 0;
+}
+// 2nd programm
+#include <iostream>
+using namespace std;
 
-// third programm
+class Student
+{
+private:
+    int rollNo;
+    string name;
+    float marks;
+
+public:
+    void input()
+    {
+        cout << "Enter Roll Number: ";
+        cin >> rollNo;
+
+        cout << "Enter Name: ";
+        cin >> name;
+
+        cout << "Enter Marks: ";
+        cin >> marks;
+    }
+
+    void display()
+    {
+        cout << "\nStudent Record\n";
+        cout << "Roll Number: " << rollNo << endl;
+        cout << "Name: " << name << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
+
+int main()
+{
+
+    Student s1;
+
+    s1.input();
+    s1.display();
+
+    return 0;
+}
+
+// 3rd programm
 #include<iostream>
 using namespace std;
 class point{
@@ -65,4 +108,40 @@ int main(){
     point p2(4,5);
     p1.show();
     p2.show();
+}
+// 4th programm
+#include <iostream>
+using namespace std;
+
+class Time
+{
+private:
+    int hh, mm, ss;
+
+public:
+    void input(int h, int m, int s)
+    {
+        hh = h;
+        mm = m;
+        ss = s;
+    }
+
+    void show()
+    {
+        cout << hh << ":" << mm << ":" << ss << endl;
+    }
+};
+
+int main()
+{
+
+    Time t1, t2;
+
+    t1.input(10, 20, 30);
+    t2.input(12, 45, 50);
+
+    t1.show();
+    t2.show();
+
+    return 0;
 }
